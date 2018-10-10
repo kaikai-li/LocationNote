bca109ebf4ef1cb7c79b6d46d5bdc0a617a249b2
Kaikai Li <likaikai1989@gmail.com>
2018-10-10 15:32:48 +0800

    Change list item style

--- note/src/main/java/com/lkk/locationnote/note/view/NoteRecyclerViewAdapter.java
+++ note/src/main/java/com/lkk/locationnote/note/view/NoteRecyclerViewAdapter.java
@@ -4,20 +4,12 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.amap.api.services.core.LatLonPoint;
-import com.amap.api.services.geocoder.GeocodeResult;
-import com.amap.api.services.geocoder.GeocodeSearch;
-import com.amap.api.services.geocoder.RegeocodeAddress;
-import com.amap.api.services.geocoder.RegeocodeQuery;
-import com.amap.api.services.geocoder.RegeocodeResult;
 import com.lkk.locationnote.common.data.NoteEntity;
-import com.lkk.locationnote.common.log.Log;
 import com.lkk.locationnote.common.utils.Util;
 import com.lkk.locationnote.note.R;
 import com.lkk.locationnote.note.R2;
@@ -55,31 +47,10 @@ public class NoteRecyclerViewAdapter extends
     @Override
     public void onBindViewHolder(@NonNull final NoteItemViewHolder holder, int position) {
         NoteEntity note = mNotes.get(position);
-        GeocodeSearch geocodeSearch = new GeocodeSearch(mContext);
-        geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
-            @Override
-            public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int code) {
-                Log.d(TAG, "onRegeocodeSearched, code= " + code);
-                // 1000为成功，其他为失败
-                if (code == 1000) {
-                    RegeocodeAddress address = regeocodeResult.getRegeocodeAddress();
-                    String location = address.getCity();
-                    if (TextUtils.isEmpty(location)) {
-                        location = address.getProvince();
-                    }
-                    holder.mLocation.setText(location);
-                }
-            }
-
-            @Override
-            public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {}
-        });
-        LatLonPoint latLonPoint = new LatLonPoint(note.getLatitude(), note.getLongitude());
-        RegeocodeQuery regeocodeQuery = new RegeocodeQuery(latLonPoint, 200, GeocodeSearch.AMAP);
-        geocodeSearch.getFromLocationAsyn(regeocodeQuery);
         holder.mTitle.setText(note.getTitle());
         holder.mTime.setText(Util.getFormatTime(mContext, note.getTime()));
         holder.mContent.setText(note.getContent());
+        holder.mLocation.setText(note.getLocation());
     }
 
     @Override
